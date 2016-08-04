c5bc402de6777264fdf6751f7b0f235cab13422d
Jonathan Hall <flimzy@flimzy.com>
2016-08-04 12:04:57 +0200

    Add Reviews

---
+++ test/review_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/flimzy/flashback-model"
+	. "github.com/flimzy/flashback-model/test/util"
+)
+
+var frozenReview []byte = []byte(`
+{
+    "cardID": "mViuXQThMLoh1G1Nlc4d_E8kR8o=.0",
+    "timestamp": null,
+    "ease": 0,
+    "interval": null,
+    "previousInterval": null,
+    "srsFactor": 0,
+    "reviewTime": null,
+    "reviewType": 0
+}
+`)
+
+func TestReview(t *testing.T) {
+	r, err := fb.NewReview("mViuXQThMLoh1G1Nlc4d_E8kR8o=.0")
+	if err != nil {
+		t.Fatalf("Error creating review: %s\n", err)
+	}
+	JSONDeepEqual(t, "Create Review", Marshal(t, "Create Review", r), frozenReview)
+
+	r2 := &fb.Review{}
+	if err := json.Unmarshal(frozenReview, r2); err != nil {
+		t.Fatalf("Error thawing review: %s\n", err)
+	}
+	JSONDeepEqual(t, "Thawed Review", Marshal(t, "Tha Review", r2), frozenReview)
+
+	if !reflect.DeepEqual(r, r2) {
+		PrintDiff(r2, r)
+		t.Fatal("Thawed and created Reviews don't match")
+	}
+}
