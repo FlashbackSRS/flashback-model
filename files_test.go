b909e8a219f970ac6eadb391a9a8cef10a747581
Jonathan Hall <flimzy@flimzy.com>
2017-08-06 17:12:36 +0200

    File collection unmarshal tests

--- files_test.go
+++ files_test.go
@@ -1,6 +1,10 @@
 package fb
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/flimzy/diff"
+)
 
 type escapeFilenameTest struct {
 	Filename string
@@ -35,12 +39,58 @@ func TestEscapeFilename(t *testing.T) {
 		if result != test.Expected {
 			t.Errorf("Escape filename '%s' failed.\n\tExpected: %s\n\t  Actual: %s\n", test.Filename, test.Expected, result)
 		}
-		unResult, err := unescapeFilename(result)
-		if err != nil {
-			t.Errorf("Unexpected error unescaping filename '%s': %s\n", unResult, err)
-		}
+		unResult := unescapeFilename(result)
 		if unResult != test.Filename {
 			t.Errorf("Unescape filename '%s' failed.\n\tExpected: %s\n\t  Actual: %s\n", result, test.Filename, unResult)
 		}
+	}
+}
+
+func TestFilesUnmarshalJSON(t *testing.T) {
+	type fujTest struct {
+		name     string
+		input    string
+		expected interface{}
+		err      string
+	}
+	tests := []fujTest{
+		{
+			name:  "bogus JSON",
+			input: "invalid",
+			err:   "invalid character 'i' looking for beginning of value",
+		},
+		{
+			name: "valid",
+			input: `{
+			"^_weirdname.txt": {
+				"content_type": "audio/mpeg",
+				"data": "YSBm"
+			},
+			"영상.jpg": {
+				"content_type": "audio/mpeg",
+				"data": "YSBL"
+			}
+		}`,
+			expected: &FileCollection{
+				files: map[string]*Attachment{
+					"_weirdname.txt": {ContentType: "audio/mpeg", Content: []byte{0x61, 0x20, 0x66}},
+					"영상.jpg":         {ContentType: "audio/mpeg", Content: []byte{0x61, 0x20, 0x4b}},
+				},
+				views: []*FileCollectionView{},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := &FileCollection{}
+			err := result.UnmarshalJSON([]byte(test.input))
+			checkErr(t, test.err, err)
+			if err != nil {
+				return
+			}
+			if d := diff.Interface(test.expected, result); d != "" {
+				t.Error(d)
+			}
+		})
 	}
 }
