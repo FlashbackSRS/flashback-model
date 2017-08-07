7be8bf0e6b64e7a22332730d63035c3ba43e534a
Jonathan Hall <flimzy@flimzy.com>
2017-08-07 11:30:42 +0200

    Update B64 encoding to not use '-' characters

--- main.go
+++ main.go
@@ -4,4 +4,8 @@ import (
 	"encoding/base64"
 )
 
-var b64encoder = base64.URLEncoding.WithPadding(base64.NoPadding)
+// We need a URL-safe encoding, but '-' has a special meaning in FB doc IDs, so
+// we use '=' instead (which is available since we don't use padding)
+const b64alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789=_"
+
+var b64encoder = base64.NewEncoding(b64alphabet).WithPadding(base64.NoPadding)
