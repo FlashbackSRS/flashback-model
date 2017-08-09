4683d723e09ade2b404d60ac5e79177f926a4060
Jonathan Hall <flimzy@flimzy.com>
2017-08-09 21:40:32 +0200

    Refactor existing package tests to be more consistent

--- package_test.go
+++ package_test.go
@@ -3,51 +3,31 @@ package fb
 import "testing"
 
 func TestPkgValidate(t *testing.T) {
-	type pvTest struct {
-		name string
-		pkg  *Package
-		err  string
-	}
-	tests := []pvTest{
+	tests := []validationTest{
 		{
 			name: "card without deck",
 			err:  "card 'abcde.mViuXQThMLoh1G1Nlc4d_E8kR8o.0' found in package, but not in a deck",
-			pkg: &Package{
+			v: &Package{
 				Cards: []*Card{
-					func() *Card {
-						c, err := NewCard("theme-VGVzdCBUaGVtZQ", 0, "card-abcde.mViuXQThMLoh1G1Nlc4d_E8kR8o.0")
-						if err != nil {
-							t.Fatal(err)
-						}
-						return c
-					}(),
+					&Card{
+						ID:      "card-abcde.mViuXQThMLoh1G1Nlc4d_E8kR8o.0",
+						ModelID: "theme-VGVzdCBUaGVtZQ/0",
+					},
 				},
 			},
 		},
 		{
 			name: "card missing from package",
 			err:  "card 'card-12345' listed in deck, but not found in package",
-			pkg: &Package{
+			v: &Package{
 				Decks: []*Deck{
-					func() *Deck {
-						return &Deck{
-							ID:    "deck-AQID",
-							Cards: &CardCollection{map[string]struct{}{"card-12345": {}}},
-						}
-					}(),
+					&Deck{
+						ID:    "deck-AQID",
+						Cards: &CardCollection{map[string]struct{}{"card-12345": {}}},
+					},
 				},
 			},
 		},
 	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			var msg string
-			if err := test.pkg.Validate(); err != nil {
-				msg = err.Error()
-			}
-			if test.err != msg {
-				t.Errorf("Unexpected error: %s", msg)
-			}
-		})
-	}
+	testValidation(t, tests)
 }
